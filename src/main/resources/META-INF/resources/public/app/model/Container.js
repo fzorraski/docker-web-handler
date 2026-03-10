8c05eda5d1bea9e3eaf8337718d7f5b45eecce7f
fzorraski <fzorraski@gmail.com>
2026-03-10 14:24:54 -0300

    remove old vanilla JS frontend
    Replace with React frontend served via Quarkus Quinoa.

--- src/main/resources/META-INF/resources/public/app/model/Container.js
+++
@@ -1,5 +0,0 @@
-export class Container {
-    constructor({containerId, image, command, created, status, ports, names}) {
-        Object.assign(this, {containerId, image, command, created, status, ports, names});
-    }
-}
